8ea17612a6b3d207cedff6b53e3a0c5a2b9b083c
CharCoder <char951144@gmail.com>
2022-10-10 23:12:59 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-
-int main(void)/*Funcion principal*/
+/*Funcion principal*/
+int main(void)
 {/*Funcion para imprimir una frase*/
   puts( "\"Programming is like building a multilingual puzzle" );
 
