8850ea5887a5f720bfd42a96c4785c798b977d5a
CharCoder <char951144@gmail.com>
2022-10-11 06:54:04 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,6 @@
 /*Funcion principal*/
 int main(void)
 {/*Funcion para imprimir una frase*/
-  puts("\"Programming is like building a multilingual puzzle");
-
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
