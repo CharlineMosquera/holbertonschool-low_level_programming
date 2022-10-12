0c171cd76db12290fcfd6ad2807de5924ca79175
CharCoder <char951144@gmail.com>
2022-10-11 22:55:13 -0700

    quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- *
+ *Descipcion funcion main
  *
  */
-int main (void)
+int main(void)/**Descripcion funcion main*/
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
-return (0);
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+return (1);
 }
