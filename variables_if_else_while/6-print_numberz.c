5b35ce1f00a63fcbb45c923d8460c1c060fa8691
CharCoder <char951144@gmail.com>
2022-10-12 16:24:04 -0700

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the text
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar("\n");
+	return (0);
+}
