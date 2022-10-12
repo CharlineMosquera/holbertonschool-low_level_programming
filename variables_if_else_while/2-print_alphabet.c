93dd761de6726184d1353cdba1ce8b9e452e0008
CharCoder <char951144@gmail.com>
2022-10-12 14:12:45 -0700

    print alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main (void)
+/* more headers goes there */
+/**
+ * main - print the text
+ *
+ * return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
 {
 	int i;
 
