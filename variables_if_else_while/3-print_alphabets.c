834658e777d0530cc93946017320f04e05c882aa
CharCoder <char951144@gmail.com>
2022-10-12 14:35:23 -0700

    alphABETS

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the text
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	{
+		putchar('\n');
+	}
+
+	return (0);
+}
