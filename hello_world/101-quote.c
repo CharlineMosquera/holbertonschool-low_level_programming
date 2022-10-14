8ffbd0f2ef8c56f726a936aaaa7e88fd1b0111b8
CharCoder <char951144@gmail.com>
2022-10-14 01:26:12 -0700

    program that prints exactly

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- *Descipcion funcion main
- *
+ *main - print text
+ *Return: 0
  */
-int main(void)/**Descripcion funcion main*/
+
+int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
