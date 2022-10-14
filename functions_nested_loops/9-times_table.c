484a153b2475d0bddefdd1c7b3f37d1bfe995861
CharCoder <char951144@gmail.com>
2022-10-14 15:38:20 -0700

    times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int a, result;
+
+	result *= a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar("result");
+		_putchar(',');
+		_putchar(' ');
+}
