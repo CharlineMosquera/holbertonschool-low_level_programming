c3f0b9c97cb19007db16316fe18fd6174c55c323
CharCoder <char951144@gmail.com>
2022-10-14 01:08:11 -0700

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- *main=funtion principal
- *
+ *main - funtion principal
+ *Return: 0
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-return (0);
+	return (0);
 }
