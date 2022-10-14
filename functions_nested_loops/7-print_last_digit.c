f43369ecd9962e36605d17db18bd55cc937ae4cb
CharCoder <char951144@gmail.com>
2022-10-14 10:21:17 -0700

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: number
+ * Return: Always 0.
+ */
+
+int print_last_digit(int i)
+{
+	int result = i;
+
+	i %= 10;
+
+	if (result < 0)
+		result *= -1;
+
+	_putchar(result + '0');
+	return (result);
+}
