6b92979d02ba7d4fff78ca9e83fd22a10aaac951
CharCoder <char951144@gmail.com>
2022-10-14 02:37:47 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include main.h
+/**
+ *_isalpha - checks for alphabetic character
+ *@c:alphabetic character
+ *Return: 0 1
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha('%c'))
+		return (1);
+
+	else
+		return (0);
+}
