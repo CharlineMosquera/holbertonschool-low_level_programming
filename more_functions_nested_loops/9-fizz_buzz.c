9601ab60a1319c51028371a0994961bff10d3dc8
CharCoder <char951144@gmail.com>
2022-10-16 16:03:55 -0700

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
