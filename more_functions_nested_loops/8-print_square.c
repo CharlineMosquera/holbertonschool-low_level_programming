9b6955111e9dfac6bcd274f2320031b9f6571f60
CharCoder <char951144@gmail.com>
2022-10-16 15:30:35 -0700

    prints a square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -20,8 +20,11 @@ void print_square(int size)
 
 		for (a = 0; a < size; a++)
 		{
-			_putchar('#');
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
