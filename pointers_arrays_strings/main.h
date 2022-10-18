514d2d2400becb7f02966a6c6497109280fba6f3
CharCoder <113852320+CharCoder@users.noreply.github.com>
2022-10-18 15:52:40 -0500

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*libaries*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-#include <ctype.h>
-
 /*prototype*/
 int _putchar(char c);
 void reset_to_98(int *n);
