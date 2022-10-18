4c07c30d82712dc58dbcf59ad87c71e711af9eed
CharCoder <char951144@gmail.com>
2022-10-17 23:11:04 -0700

    prints a string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,17 @@
 
 void print_rev(char *s)
 {
-	int c, r;
+	int c, r, cont = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
-		for (r = c; r >= 0; r--)
-		{
-			_putchar(r);
-		}
+		cont++;
 	}
+
+	for (r = (cont - 1); r >= 0; r--)
+	{
+		_putchar(s[r]);
+	}
+
 	_putchar('\n');
 }
