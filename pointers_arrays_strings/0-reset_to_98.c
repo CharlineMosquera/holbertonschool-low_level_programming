bef8bbeebacdea6c6251bd4de4a6405d3394e251
CharCoder <char951144@gmail.com>
2022-10-17 19:22:36 -0700

    function that takes a pointer

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * reset_to_98(int *n) - function that takes a pointer
+ * reset_to_98 - function that takes a pointer
  * @n: pointer
+ * Return: 0
  */
 
 void reset_to_98(int *n)
