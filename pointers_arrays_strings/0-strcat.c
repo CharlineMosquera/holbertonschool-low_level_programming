42b8854aab0a3b96ba9cf4ac64715738c20a960d
CharCoder <char951144@gmail.com>
2022-10-19 15:56:43 -0700

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: string
+ * @src: string
+ * Return: @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a+=1)
+	{
+	}
+	for (b = 0; src[b] != '\0'; b+=1)
+	{
+		dest[a] = src[b];
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
