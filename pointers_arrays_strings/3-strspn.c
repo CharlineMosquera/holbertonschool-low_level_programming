f31f08eabe749651e5b61bd93215399d0c7bfc07
CharCoder <char951144@gmail.com>
2022-10-21 18:34:25 -0700

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _strspn - function that gets the length of a prefix substring
+ * _strspn - function that gets the length of a prefix substring
  * @s: pointer
  * @accept: pointer
  * Return: 0
