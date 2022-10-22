179465d065477bcb645948fee17921683c33f4d7
CharCoder <char951144@gmail.com>
2022-10-21 18:18:03 -0700

    strchr

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (; i < n;)
+	for (i = 0; i < n; i++)
 	{
-		i++;
 		dest[i] = src[i];
 	}
 	return (dest);
