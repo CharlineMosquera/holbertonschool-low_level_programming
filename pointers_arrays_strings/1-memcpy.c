0d3dcde1c9363f84d1de103596ddc9dae259b3a4
CharCoder <char951144@gmail.com>
2022-10-21 17:54:35 -0700

    memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ * @dest: pointer
+ * @src: pointer
+ * @n: bytes from memory area
+ * Return: @dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n;)
+	{
+		i++;
+		dest[i] = src[i];
+	}
+	return (dest);
+}
