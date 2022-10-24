7fad1c7d89f7f1fa16953b3eae4c63020ffd3fb9
CharCoder <char951144@gmail.com>
2022-10-24 10:16:42 -0700

    prints a string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s = '\0')
+	{
+		return;
+	}
+}
+
+int main (void)
+{
+	_puts_recursion(*s);
+	return (0);
+}
