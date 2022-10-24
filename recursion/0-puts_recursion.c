17cb4592a99d08a767e6fe1b49da1616064b2040
CharCoder <char951144@gmail.com>
2022-10-24 13:19:44 -0700

    puts recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,14 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (s = '\0')
+	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
-}
-
-int main (void)
-{
-	_puts_recursion(*s);
-	return (0);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
