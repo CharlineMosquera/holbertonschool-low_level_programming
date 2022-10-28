b2dea2c976439c1036e8533e2a80842eba758a88
CharCoder <char951144@gmail.com>
2022-10-28 15:46:18 -0700

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,12 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	int i, mul = 0;
+	int mul = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argc == 3)
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 
 	return (0);
