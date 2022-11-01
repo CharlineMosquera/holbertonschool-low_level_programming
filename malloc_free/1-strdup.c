e050545b474b10ea7273f8c377bf5e1cbc766bf8
CharCoder <char951144@gmail.com>
2022-11-01 11:12:11 -0700

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strdup - function that returns a pointer to a newly
+ * allocated space in memory
+ * @str: string
+ * Return: pointer or NULL if failed
+ */
+
+char *_strdup(char *str)
+{
+	char *dup = NULL;
+	int cont = 0, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[cont] != '\0')
+		cont++;
+
+	dup = malloc(sizeof(char) * cont + 1);
+
+	for (; len <= cont; len++)
+		dup[len] = str[len];
+
+	return (dup);
+
+	free(dup);
+	return (0);
+}
