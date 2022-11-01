018587cff93fc67dafef47961b37d707ccd79c89
CharCoder <char951144@gmail.com>
2022-11-01 09:44:50 -0700

    Create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,14 +13,18 @@ char *create_array(unsigned int size, char c)
 	char *arr = NULL;
 	unsigned int i;
 
-	if (arr == NULL || size == 0)
+	if (size == 0)
 		return (NULL);
 
 	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 	return (arr);
 
-
+	free(arr);
+	return(0);
 }
