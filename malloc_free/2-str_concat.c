c5396545e0af406bcb61434884a7024e4f7f9493
CharCoder <char951144@gmail.com>
2022-11-01 20:14:48 -0700

    str concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,10 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int len1 = 0, len2 = 0, i = 0;
 
 	if (s1 == NULL)
-		return ('\0');
+		s1 = "";
 
 	if (s2 == NULL)
-		return ('\0');
+		s2 = "";
 
 	while (s1[len1] != '\0')
 		len1++;
