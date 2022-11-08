9a8c5d58829a3058f24ccdc960f519559c838b58
CharCoder <char951144@gmail.com>
2022-11-08 11:19:47 -0800

    C - Structures, typedef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+
+/**
+ * dog - tag name
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: dog data record
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
