ac07a9d617948d0d885d1cacb46a456f5cdeeb96
CharCoder <char951144@gmail.com>
2022-11-08 11:37:26 -0800

    Poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * dog - tag name
+ * struct dog - tag name
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner
@@ -14,3 +16,5 @@ struct dog
 	float age;
 	char *owner;
 };
+
+#endif /*DOG_H*/
