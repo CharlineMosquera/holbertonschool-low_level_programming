4a7f83a45f3c92eb6761adba402dd105fd4aa80f
CharCoder <char951144@gmail.com>
2022-11-10 13:04:21 -0800

    print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	(d->name == NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
-	(d->age == 0) ? printf("Age: 0.00\n") : printf("Age: %f\n", d->age);
+	(d->age == 0) ? printf("Age: 0.000000\n") : printf("Age: %f\n", d->age);
 	(on == NULL) ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 }
