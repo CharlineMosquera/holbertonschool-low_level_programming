7c7fbdb9cd8605f6a369475496f41b7d5851ca74
CharCoder <char951144@gmail.com>
2022-12-02 18:16:12 -0500

    change name of variable

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,13 +8,13 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t len = 0;
+	size_t count_nodes = 0;
 
-	while (h)
+	while (h) 
 	{
-		len++;
+		count_nodes++;
 		h = h->next;
 	}
 
-	return (len);
+	return (count_nodes);
 }
