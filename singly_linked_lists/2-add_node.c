1f78d993b186502efb34190085afb521f4f37e66
CharCoder <char951144@gmail.com>
2022-12-02 18:01:52 -0500

    clean code add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: 0
+ */
+
+int _strlen(const char *s)
+{
+	int cont = 0;
+
+	while (s[cont] != '\0')
+	{
+		cont++;
+	}
+	return (cont);
+}
+
+/**
  * add_node - function that adds a new node at the beginning of a list_t list.
  * @head: nodo
  * @str: str
@@ -10,28 +27,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_nodo;
-	char *aux;
-	int len = 0;
 
 	new_nodo = (list_t *)malloc(sizeof(list_t));
-	if (new_nodo != NULL)
-	{
-		aux = strdup(str);
+	if (new_nodo == NULL)
+		return (NULL);
 
-		if (aux == NULL)
-		{
-			free(new_nodo);
-			return (NULL);
-		}
-		while (str[len])
-		{
-			len++;
-		}
-		new_nodo->str = aux;
-		new_nodo->len = len;
-		new_nodo->next = *head;
-		*head = new_nodo;
-		return (new_nodo);
-	}
-	return (NULL);
+	new_nodo->str = strdup(str);
+	new_nodo->len = _strlen(str);
+	new_nodo->next = *head;
+	*head = new_nodo;
+	return (new_nodo);
 }
