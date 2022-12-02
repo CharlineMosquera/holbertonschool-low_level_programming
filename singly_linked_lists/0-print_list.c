1f69b7682552a73d5f8516fd2408f634c8025941
CharCoder <char951144@gmail.com>
2022-12-02 13:55:34 -0500

    add Print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t
+ * @h: list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t count_nodos = 0;
+
+    if (!h)
+        return count_nodos;
+
+    while (h)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+        count_nodos++;
+        h = h->next;
+    }
+
+    return (count_nodos);
+}
