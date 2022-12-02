919b32036dd51d676ce1ee901b3b62c647a51f48
CharCoder <char951144@gmail.com>
2022-12-02 14:53:45 -0500

    print list Betty Style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,20 +8,18 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t count_nodos = 0;
+	size_t count_nodos = 0;
 
-    if (!h)
-        return count_nodos;
-
-    while (h)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        count_nodos++;
-        h = h->next;
-    }
-
-    return (count_nodos);
+	if (!h)
+		return count_nodos;
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count_nodos++;
+		h = h->next;
+	}
+	return (count_nodos);
 }
