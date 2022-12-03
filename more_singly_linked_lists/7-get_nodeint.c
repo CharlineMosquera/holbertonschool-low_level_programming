3955ae51f95a1f04d5f827c715db6ddcaf192da1
CharCoder <char951144@gmail.com>
2022-12-02 20:49:06 -0500

    get node int

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int get_node = 0;
 
-	while (get_node <= index)
+	while (get_node < index)
 	{
 		if (head == NULL)
 			return (NULL);
-
 		head = head->next;
 		get_node++;
 	}
