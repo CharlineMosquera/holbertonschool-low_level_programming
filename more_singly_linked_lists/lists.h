b2df9786ea25d1235625eee12d565990145d20fe
CharCoder <char951144@gmail.com>
2022-12-02 21:23:31 -0500

    list.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LIST_H
+#define LIST_H
 
 /**
  * struct listint_s - singly linked list
@@ -31,4 +31,4 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
-#endif /*MAIN_H*/
+#endif /*LIST_H*/
