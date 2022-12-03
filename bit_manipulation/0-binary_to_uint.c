b14f01399c55470b6f71b21e1eeb2ef7a2cc9312
CharCoder <char951144@gmail.com>
2022-12-02 22:22:35 -0500

    binary to uint

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <math.h>
+#include <stddef.h>
 
 /**
  * _strlen - function that returns the length of a string
@@ -29,12 +29,13 @@ unsigned int binary_to_uint(const char *b)
 	int i, len, exp = 1;
 	char valor;
 
+	if (b == NULL)
+		return (0);
+
 	len = _strlen(b);
 
 	for (i = len - 1; i >= 0; i--)
 	{
-		if (b[i] == '\0')
-			return (0);
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		valor = b[i];
