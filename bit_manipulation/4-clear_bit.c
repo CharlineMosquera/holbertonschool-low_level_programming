de52071475677b97877d3e09df97efa6df992d69
CharCoder <char951144@gmail.com>
2022-12-05 21:47:07 -0500

    clear bit

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,8 +9,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index > (sizeof(unsigned long int) * 8))
-        return (-1);
-    *n ^= (0 << index);
-    return (1);
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n &= (0 << index);
+	return (1);
 }
