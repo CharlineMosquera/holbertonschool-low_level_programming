25d140bdccfed1100464d14bcaa4afea139492b0
CharCoder <char951144@gmail.com>
2022-12-09 23:21:36 -0500

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fildes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fildes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fildes == -1)
 		return (-1);
 	write(fildes, text_content, len);
