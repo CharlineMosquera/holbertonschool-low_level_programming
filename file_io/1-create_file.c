f4c8c253aaf466b67c82893c8678974c97ee1182
CharCoder <char951144@gmail.com>
2022-12-09 23:28:35 -0500

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@ int _strlen(const char *s)
 {
 	int cont = 0;
 
-	while (s[cont] != '\0')
+	while (s[cont])
 	{
 		cont++;
 	}
@@ -28,9 +28,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
-	if (text_content == NULL)
-		len = '\0';
 
 	fildes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fildes == -1)
