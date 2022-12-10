a6907bb46461a839470532d261010beffa94e309
CharCoder <char951144@gmail.com>
2022-12-09 21:38:30 -0500

    add libraries

--- file_io/main.h
+++ file_io/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*prototypes*/
+/*LIBRARIES*/
+#include <fcntl.h> /*to use function open*/
+#include <unistd.h> /*to use function read, close*/
+#include <stdlib.h> /*to use function malloc*/
+
+/*PROTOTYPES*/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
