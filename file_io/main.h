8036d1e2b2cd43c1f77ecb2514cf68ca02dab07f
CharCoder <char951144@gmail.com>
2022-12-09 23:15:45 -0500

    add prototype _strlen

--- file_io/main.h
+++ file_io/main.h
@@ -2,13 +2,15 @@
 #define MAIN_H
 
 /*LIBRARIES*/
+#include <stdio.h>
 #include <fcntl.h> /*to use function open*/
-#include <unistd.h> /*to use function read, close*/
+#include <unistd.h> /*to use function read, close, write*/
 #include <stdlib.h> /*to use function malloc*/
 
 /*PROTOTYPES*/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _strlen(const char *s);
 
 #endif /*MAIN_H*/
