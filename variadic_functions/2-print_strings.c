c44d06b375b7599c842ceea29ccd5c65a86bd196
CharCoder <char951144@gmail.com>
2022-12-11 02:08:42 -0500

    print string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			string = va_arg(args, char *);
 			if (string == NULL)
-				printf("(nill)");
+				printf("(nil)");
 			else
 				printf("%s", string);
 
