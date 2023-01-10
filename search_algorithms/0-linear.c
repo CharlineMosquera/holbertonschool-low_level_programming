76a6d27be411349fa6ab421c8c050beee7558a60
CharlineMosquera <char951144@gmail.com>
2023-01-09 22:57:41 -0500

    linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,19 +11,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t index;
+	size_t index = 0;
 
 	if (array == NULL || !value)
 		return (-1);
-
-	for (index = 0; index < size; index++)
+	while (index < size)
 	{
-		if (array[index] == value)
-		{
-			printf("Value checked array[%ld] = [%d]\n", index, array[index]);
-			break;
-		}
 		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+		index++;
 	}
-	return (index);
+	return (-1);
 }
